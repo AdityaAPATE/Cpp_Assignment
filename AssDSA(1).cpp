af33a0a248ec3b2cd86393b134ad0eb059e14eee
AdityaAPATE <115172879+AdityaAPATE@users.noreply.github.com>
2022-10-29 20:02:51 +0530

    Update AssDSA(1).cpp

--- AssDSA(1).cpp
+++ AssDSA(1).cpp
@@ -11,3 +11,26 @@ cout<<area_of_circle(14)<<endl;
 cout<<area_of_circle(5);
 getch();
 }
+
+//   // Below is Factorial code
+//   #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     long factorial = 1.0;
+
+//     cout << "Enter a positive integer: ";
+//     cin >> n;
+
+//     if (n < 0)
+//         cout << "Error! Factorial of a negative number doesn't exist.";
+//     else {
+//         for(int i = 1; i <= n; ++i) {
+//             factorial *= i;
+//         }
+//         cout << "Factorial of " << n << " = " << factorial;    
+//     }
+
+//     return 0;
+// }
